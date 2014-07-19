f9337f900e20dec6cbbcf5f3bf1592fc84bce414
Scott Pennington <makeitgo@gmail.com>
2014-07-18 19:15:37 -0700

    adjusted some configurations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'tic_tac_toe#index'
+  get 'tic_tac_toe/index'
+
+  get 'tic_tac_toe/turn'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
